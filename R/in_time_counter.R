7e46032c4fe67420a937c5d1137e8f03c18a8f2f
John MacKintosh <johnmackintosh@users.noreply.github.com>
2019-12-17 21:42:01 +0000

    Update and rename base_counter.R to in_time_counter.R

--- R/in_time_counter.R
+++ R/in_time_counter.R
@@ -1,4 +1,4 @@
-#' base_counter
+#' in_time_counter
 #'
 #' @param df dataframe, tibble or data.table
 #' @param identifier unique patient identifier
@@ -21,50 +21,52 @@ in_time_counter <- function(df,
                             group_var,
                             time_unit = "1 hour",
                             summarise = FALSE) {
-
+  
   ##### check all arguments provided #####
-
+  
   if (missing(df)) {stop("Please provide a value for df")}
-
+  
   if (missing(identifier)) {stop("Please provide a value for the patient identifier")}
-
+  
   if (missing(admit_datetime)) {stop("Please provide a value for admit_datetime")}
-
+  
   if (missing(discharge_datetime)) {stop("Please provide a value for discharge_datetime")}
-
+  
   if (missing(group_var)) {stop("Please provide a value for the group_var column")}
-
+  
   #if (missing(time_unit)) {stop("Please provide a value for time_unit. See '? seq.POSIXt' for valid values")}
-
+  
   ##### end of check #####
-
-  DT <- setDT(df)
-
-
+  
+  DT <- setDT(copy(df))
+  keycols <- c(identifier, admit_datetime, discharge_datetime)
+  
+  setkeyv(DT, keycols)
+  
+  
   is.POSIXct <- function(x) inherits(x, "POSIXct")
-
+  
   if (DT[, !is.POSIXct(get(admit_datetime))]) {
     stop("The admit_datetime column must be POSIXct")
   }
-
+  
   if (DT[, !is.POSIXct(get(discharge_datetime))]) {
     stop("The discharge_datetime column must be POSIXct")
   }
-
+  
   maxdate <- DT[, max(get(discharge_datetime), na.rm = TRUE)]
-
+  
   setnafill(DT, type = "const", fill = maxdate, cols = discharge_datetime)
-
+  
   DT[, join_start := lubridate::floor_date(get(admit_datetime), unit = time_unit)]
   DT[, join_end := lubridate::ceiling_date(get(discharge_datetime), unit = time_unit)]
-
-
+  
+  
   pat_res <- DT[, .(in_time = seq(join_start, join_end, by = time_unit)),
                 by = .(identifier = get(identifier),
                        group_var = get(group_var),
                        ID = seq_len(nrow(DT)))][order(in_time)]
-
-
+  
   if (!summarise) {
     return(pat_res)
   }
