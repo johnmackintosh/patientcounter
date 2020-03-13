60bb7e275547668f4a8ed6e4b71506d0502355ed
John MacKintosh <johnmackintosh@gmx.com>
2020-03-13 13:34:18 +0000

    revise logo

--- data-raw/beds.R
+++ data-raw/beds.R
@@ -1,54 +1,11 @@
 beds <-
-  structure(
-    list(
-      bed = c("A", "A", "A", "A", "B", "B", "B", "B",
-              "C", "D"),
-      patient = c(
-        "Person1",
-        "Person5",
-        "Person9",
-        "Person8",
-        "Person2",
-        "Person6",
-        "Person7",
-        "Person10",
-        "Person3",
-        "Person4"
-      ),
-      start_time = structure(
-        c(
-          1577867640,
-          1577872500,
-          1577874840,
-          1577898000,
-          1577868300,
-          1577891580,
-          1577911308,
-          1577916780,
-          1577869500,
-          1577871000
-        ),
-        class = c("POSIXct", "POSIXt"),
-        tzone = "UTC"
-      ),
-      end_time = structure(
-        c(
-          1577871240,
-          1577873724,
-          1577982840,
-          1578006000,
-          1577886300,
-          1577910444,
-          1577915772,
-          1577922180,
-          1577871300,
-          NA
-        ),
-        class = c("POSIXct", "POSIXt"),
-        tzone = "UTC"
-      )
-    ),
-    row.names = c(NA,
-                  -10L),
+  structure(list(bed = c("A", "A", "A", "A", "B", "B", "B", "B",
+                         "C", "D"), patient = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), start_time = structure(c(1577871240,
+                                                                                                         1577876100, 1577878440, 1577901600, 1577871900, 1577895180, 1577914908,
+                                                                                                         1577920380, 1577873100, 1577874600), class = c("POSIXct", "POSIXt"
+                                                                                                         ), tzone = "UTC"), end_time = structure(c(1577874840, 1577877324,
+                                                                                                                                                   1577986440, 1578009600, 1577889900, 1577914044, 1577919372, 1577925780,
+                                                                                                                                                   1577874900, NA), class = c("POSIXct", "POSIXt"), tzone = "UTC")), row.names = c(NA,
+                                                                                                                                                                                                                                   -10L),
     class = c("tbl_df", "tbl", "data.frame")
   )
